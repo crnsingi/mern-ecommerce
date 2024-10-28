4166c08ab6d64b9408eb91ca7e6ec659daa51e05
Cesar <cesarnsingi@hotmail.com>
2024-10-28 21:36:50 +0400

    import image

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,8 +6,9 @@ import cart_icon from '../Assets/cart_icon.png'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <div className="nav-logo"></div>
-
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+        </div>
     </div>
   )
 }
