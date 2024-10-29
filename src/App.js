a8aabdab7f9f3e416b7b70b76f5bf0f6ea4586c6
Cesar <cesarnsingi@hotmail.com>
2024-10-30 01:17:57 +0400

    add Route path

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop/>} />
 
 
-         </BrowserRouter>
-
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
