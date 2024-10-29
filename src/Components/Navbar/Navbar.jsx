c18e7cb2eb21d5460cd766aeb445c45645b25378
Cesar <cesarnsingi@hotmail.com>
2024-10-30 00:05:46 +0400

    add hr to all on click

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => {
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<h/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}>Men{menu==="shop"?<h/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}>Women{menu==="shop"?<h/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="shop"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className="nav-login-cart">
             <button>Login</button>
