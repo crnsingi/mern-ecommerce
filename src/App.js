d567fdb72970dc8b34c713ee066d5f4e8ce05727
Cesar <cesarnsingi@hotmail.com>
2024-10-30 01:10:08 +0400

    add browser router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      
       <Navbar />
-   
+
+
+         </BrowserRouter>
+
     </div>
   );
 }
