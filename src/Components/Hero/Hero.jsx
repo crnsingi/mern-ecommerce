814c29f265d9cc13c4b6e1a178996d0baab09be0
Cesar <cesarnsingi@hotmail.com>
2024-11-01 04:28:58 +0400

    add div and h2

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,13 @@ import './Hero.css'
 const Hero = () => {
   return (
     <div className='hero'>
+        <div className="hero-left">
+            <h2>NEW ARRIVALS ONLY</h2>
 
+        </div>
+        <div className="hero-right">
+
+        </div>
     </div>
   )
 }
