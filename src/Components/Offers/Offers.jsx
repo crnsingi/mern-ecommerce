dc2a30dd22a43c15c65259cb3e381d0b48441794
Cesar <cesarnsingi@hotmail.com>
2024-11-05 22:24:39 +0100

    add paragraph Offers

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -5,7 +5,9 @@ const Offers = () => {
   return (
     <div className='offers'>
         <div className="offers-left">
-            <h1></h1>
+            <h1>Exclusive</h1>
+            <h1>Offers For You</h1>
+            <p></p>
         </div>
         <div className="div offers-right">
 
