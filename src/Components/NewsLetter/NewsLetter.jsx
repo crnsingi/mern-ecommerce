9106b5b804aac6760cc4c636c58f126c266005a0
Cesar <cesarnsingi@hotmail.com>
2024-11-06 06:34:39 +0100

    add div and input

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -6,7 +6,9 @@ const NewsLetter = () => {
     <div className='newsletter'>
         <h1>Get Exclusive Offers On Your Email</h1>
         <p>Subscribe to our newsletter and stay updated</p>
-
+        <div>
+            <input type="text" />
+        </div>
     </div>
   )
 }
