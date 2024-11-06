a21fad000878421cf9ce5834424c311429877077
Cesar <cesarnsingi@hotmail.com>
2024-11-06 20:16:18 +0100

    add the pintester and whatsapp

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -22,7 +22,12 @@ const Footer = () => {
         <div className="footer-social-icon">
             <div className="footer-icons-container">
                 <img src={instagram_icon} alt="" />
-
+            </div>
+            <div className="footer-icons-container">
+                <img src={pintester_icon} alt="" />
+            </div>
+            <div className="footer-icons-container">
+                <img src={whatsapp_icon} alt="" />
             </div>
         </div>
     </div>
