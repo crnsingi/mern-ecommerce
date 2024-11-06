704abadfe029a3ee211a3e29f7afeae4b5ddab75
Cesar <cesarnsingi@hotmail.com>
2024-11-06 04:42:58 +0100

    add paragraph to News Letter

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -5,6 +5,7 @@ const NewsLetter = () => {
   return (
     <div className='newsletter'>
         <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Subscribe to our newsletter and stay updated</p>
 
     </div>
   )
