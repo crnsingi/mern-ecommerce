de1460e6e08c8d47d9cc3c63f5861a6ba923bc58
Cesar <cesarnsingi@hotmail.com>
2024-11-06 20:03:15 +0100

    import pinstester

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Footer.css'
 import footer_logo from '../Assets/logo_big.png'
 import instagram_icon from '../Assets/instagram_icon.png'
+import pintester_icon from '../Assets/pintester_icon.png'
 
 const Footer = () => {
   return (
