193025b97fa32cd6eb5ce53a6851c4df49536403
Cesar <cesarnsingi@hotmail.com>
2024-11-06 03:25:22 +0100

    add div className to collections

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -6,7 +6,9 @@ const NewCollections = () => {
     <div className='new-collections'>
         <h1>NEW COLLECTIONS</h1>
         <hr />
+        <div className="collections">
 
+        </div>
     </div>
   )
 }
