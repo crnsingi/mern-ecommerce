6a54efe6434348db7b0aea36f21bd92a1675debd
Cesar <cesarnsingi@hotmail.com>
2024-11-06 20:01:49 +0100

    import instagram icon

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Footer.css'
 import footer_logo from '../Assets/logo_big.png'
+import instagram_icon from '../Assets/instagram_icon.png'
 
 const Footer = () => {
   return (
