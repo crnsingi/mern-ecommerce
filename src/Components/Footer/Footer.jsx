f1ad7606cb3297808a9fdfd0b4c121d906097433
Cesar <cesarnsingi@hotmail.com>
2024-11-06 20:04:58 +0100

    import whatsapp icon

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import './Footer.css'
 import footer_logo from '../Assets/logo_big.png'
 import instagram_icon from '../Assets/instagram_icon.png'
 import pintester_icon from '../Assets/pintester_icon.png'
+import whatsapp_icon from '../Assets/whatsapp_icon.png'
 
 const Footer = () => {
   return (
