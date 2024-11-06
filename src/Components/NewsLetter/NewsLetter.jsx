bac238dd81fe1b4ad3fe0acfd55aa91307566b99
Cesar <cesarnsingi@hotmail.com>
2024-11-06 06:49:28 +0100

    add button to News Letter

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -7,7 +7,8 @@ const NewsLetter = () => {
         <h1>Get Exclusive Offers On Your Email</h1>
         <p>Subscribe to our newsletter and stay updated</p>
         <div>
-            <input type="text" />
+            <input type="email" placeholder='Your Email id' />
+            <button></button>
         </div>
     </div>
   )
