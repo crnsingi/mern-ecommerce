1ac89362f2ca32b4dc6cafee9f0097b9b823da50
Cesar <cesarnsingi@hotmail.com>
2024-11-07 07:04:13 +0100

    import all product

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,9 +1,10 @@
 import React, { createContext } from "react";
+import all_product from "../Components/Assets/all_product"
 
 
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => { 
 
-     const ContextValue = {}
+     const contextValue = {}
 }
