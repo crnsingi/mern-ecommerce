a4d4c8588575d2bb3326be566c90d41766f591ae
Cesar <cesarnsingi@hotmail.com>
2024-11-07 04:38:49 +0100

    add myema copyright

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
         </div>
         <div className="footer-copyright">
             <hr />
-            <p>Copyright @ 2023- All Right Reserved.</p>
+            <p>Copyright @ 2024 myema.ao - All Right Reserved.</p>
         </div>
     </div>
   )
