ecd656cdecb2e407c90ae5517156d03c0be79ffb
Cesar <cesarnsingi@hotmail.com>
2024-11-09 00:46:50 +0100

    add props category

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -18,8 +18,8 @@ const ShopCategory = (props) => {
       </div>
       <div className="shopcategory-products">
         {all_product.map((item,i)=>{
-          if(){
-            
+          if(props.category===item.category){
+
           }
 
         })}
