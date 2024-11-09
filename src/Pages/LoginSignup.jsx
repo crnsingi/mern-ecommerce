b55a40b720b991b681ebfb68617210774062fef0
Cesar <cesarnsingi@hotmail.com>
2024-11-09 14:15:37 +0100

    add h1 Sign Up

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const LoginSignup = () => {
   return (
     <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
 
-        
+      </div>
     </div>
   )
 }
