cbdf62960241d81fc3b2bcfc2a1d2bcda05b4972
Cesar <cesarnsingi@hotmail.com>
2024-11-09 14:34:50 +0100

    add input type

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -6,7 +6,9 @@ const LoginSignup = () => {
       <div className="loginsignup-container">
         <h1>Sign Up</h1>
         <div className="loginsignup-fields">
-
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address'/>
+          <input type="password" placeholder='Password' />
         </div>
       </div>
     </div>
