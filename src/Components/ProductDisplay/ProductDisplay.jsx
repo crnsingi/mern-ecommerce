cd0be47626b0df7eb1546747f3ffeabef4186df7
Cesar <cesarnsingi@hotmail.com>
2024-11-12 15:28:52 +0100

    add productdisplay img

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './ProductDisplay.css'
 
-const ProductDisplay = () => {
+const ProductDisplay = (props) => {
+    const {product} = props;
   return (
     <div className='productdisplay'>
         <div className="productdisplay-left">
          <div className="productdisplay-img-list">
-            <img src="" alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+         </div>
+         <div className="productdisplay-img">
 
          </div>
         </div>
