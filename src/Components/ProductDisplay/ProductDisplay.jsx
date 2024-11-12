8b046e7a8ab881d09b836293ce8d271ff22aa37f
Cesar <cesarnsingi@hotmail.com>
2024-11-12 16:56:03 +0100

    add tags

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -49,6 +49,8 @@ const ProductDisplay = (props) => {
             </div>
             <button>ADD TO CART</button>
             <p className='productdisplay-right-category'><span>Category :</span>Women , T-Shirt, Crop Top</p>
+            <p className='productdisplay-right-category'><span>Tags :</span>Modern , Latest</p>
+            
         </div>
     </div>
   )
