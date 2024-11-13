6563cce17f957559c2ded3d5787b648454452ef4
Cesar <cesarnsingi@hotmail.com>
2024-11-13 19:06:39 +0100

    add div and Description

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -4,7 +4,10 @@ import './DescriptionBox.css'
 const DescriptionBox = () => {
   return (
     <div className='descriptionbox'>
-      
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+
+        </div>
     </div>
   )
 }
