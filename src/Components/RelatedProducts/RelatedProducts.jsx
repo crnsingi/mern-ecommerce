502bc2a1db0e929747929c5f23def8feef891a8d
Cesar <cesarnsingi@hotmail.com>
2024-11-13 20:54:01 +0100

    import data product

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './RelatedProducts.css'
+import data_product from '../Assets/data'
 
 const RelatedProducts = () => {
   return (
     <div className='relatedproducts'>
         <h1>Related Products</h1>
-      
+        <hr />
+        <div className="relatedproducts-item">
+
+        </div>
     </div>
   )
 }
