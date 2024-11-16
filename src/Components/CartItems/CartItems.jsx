9dad83dc47ea23a5a3ba85890ab8419d75cc9a93
Cesar <cesarnsingi@hotmail.com>
2024-11-16 18:30:42 +0100

    add car item promo code

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -49,9 +49,13 @@ const CartItems = () => {
                 <hr />
                 <div className="cartitems-total-item">
                     <h3>Total</h3>
-
+                    <h3>${0}</h3>
                 </div>
             </div>
+            <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+            
         </div>
     </div>
     </div>
