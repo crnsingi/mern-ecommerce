584b812c3df2b4ca0653397dcc414e4ddb1ba6ad
Cesar <cesarnsingi@hotmail.com>
2024-11-16 12:34:01 +0100

    add class Name

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './CartItems.css'
+import { ShopContext } from '../../Context/ShopContext'
 
 const CartItems = () => {
+    const {all_product,CartItems,removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
+        <div className="cartitems-format-main"></div>
       
     </div>
   )
