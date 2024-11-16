5c5965df826bb5478e2cbf5a4ece651f04aba0fa
Cesar <cesarnsingi@hotmail.com>
2024-11-16 13:57:18 +0100

    cart items property

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../../Context/ShopContext'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-    const {all_product,CartItems,removeFromCart} = useContext(ShopContext);
+    const {all_product,cartItems,removeFromCart} = useContext(ShopContext);
   return (
     <div className='cartitems'>
         <div className="cartitems-format-main">
@@ -17,16 +17,16 @@ const CartItems = () => {
     </div>
     <hr />
     {all_product.map((e)=>{
-        if(CartItems[e.id]>0)
+        if(cartItems[e.id]>0)
         {
             return <div>
             <div className="cartitems-format">
             <img src={e.image} alt="" className='carticon-product-icon' />
             <p>{e.name}</p>
-            <p>{e.new_price}</p>
-            <button className='cartitems-quantity'>{CartItems[e.id]}</button>
-            <p>{e.new_price*CartItems[e.id]}</p>
-            <img src={remove_icon} onClick={()=>{removeFromCart()}} alt="" />
+            <p>${e.new_price}</p>
+            <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+            <p>{e.new_price*cartItems[e.id]}</p>
+            <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
         </div>
         <hr />
         </div>
