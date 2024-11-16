14e95354041a5e81e19f5b5833057e03981ae6c6
Cesar <cesarnsingi@hotmail.com>
2024-11-16 11:04:48 +0100

    for with all product length

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -10,8 +10,8 @@ const ShopContextProvider = (props) => {
 
      const getDefaultCart = ()=>{
         let cart = {};
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
+        for (let index = 0; index < all_product.length; index++) {
+            cart[index] = 0;
             
         }
 
