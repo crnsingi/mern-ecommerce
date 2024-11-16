48b47aada38fc783ebd2c7c85cb67e28e5995000
Cesar <cesarnsingi@hotmail.com>
2024-11-16 11:37:39 +0100

    add property to context value

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -24,7 +24,7 @@ const ShopContextProvider = (props) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
      }
 
-     const contextValue = {all_product,cartItems};
+     const contextValue = {all_product,cartItems,addToCart,removeFromCart};
 
     
     
