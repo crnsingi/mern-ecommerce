acbca0804916c2f5a4c6f59e009fc09b1acebbfa
Cesar <cesarnsingi@hotmail.com>
2024-11-21 09:14:39 +0100

    database connection with MongoDB

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,6 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
+//Database connection with MongoDB
+mongoose.connect("mongodb+srv://cesarricardo:Nicaise93$@cluster0.aequl.mongodb.net/e-commerce")
