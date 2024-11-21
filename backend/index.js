c58d0dbd8c8e5bc54edd890b2d98096ae522839a
Cesar <cesarnsingi@hotmail.com>
2024-11-21 08:44:19 +0100

    add multer require

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,5 @@ const port = 4000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
