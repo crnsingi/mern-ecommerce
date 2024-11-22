7cb32e6cf1c0f0b0ffa3487010a07a4bb69f2647
Cesar <cesarnsingi@hotmail.com>
2024-11-22 19:21:24 +0100

    add input

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -6,6 +6,7 @@ const AddProduct = () => {
     <div className='add-product'>
         <div className="addproduct-itemfield">
             <p>Product title</p>
+            <input type="text"  name='name' placeholder='Type here'/>
         </div>
     </div>
   )
