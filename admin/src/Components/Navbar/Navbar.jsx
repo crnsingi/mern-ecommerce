0c81f1ac170918be95797f98e38ec027f8e9d6b5
Cesar <cesarnsingi@hotmail.com>
2024-11-22 14:18:36 +0100

    add navProfile

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Navbar.css'
 import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src="" alt="" className="nav-logo" />
+        <img src={navlogo} alt="" className="nav-logo" />
+        <img src={} alt="" />
     </div>
   )
 }
