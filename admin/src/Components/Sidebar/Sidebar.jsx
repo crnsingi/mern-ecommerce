61e8bca05275e07b26b81331d3bac94e0caa7b07
Cesar <cesarnsingi@hotmail.com>
2024-11-22 16:55:47 +0100

    import add product icon

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Sidebar.css'
 import {Link} from 'react-router-dom'
+import add_product_icon from '../../assets/Product_Cart.svg'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <Link to={'/addproduct'} style={{textDecoration:"none"}}>
+        <img src="" alt="" />
+        <div className="sidebar-item">
 
+        </div>
         </Link>
     </div>
   )
