ecbe7813086986c7101ba9622d52e23e7f735429
Cesar <cesarnsingi@hotmail.com>
2024-11-22 18:54:39 +0100

    add eslintrc

--- admin/eslint.config.js
+++ admin/eslint.config.js
@@ -24,6 +24,8 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
+      "react/jsx-uses-react": 2,   
+      "react/jsx-uses-vars": "error" ,
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
@@ -36,3 +38,5 @@ export default [
     },
   },
 ]
+
+
