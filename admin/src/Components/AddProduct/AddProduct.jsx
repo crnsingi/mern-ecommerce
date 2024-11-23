e9eaf2e6d0e5b586b7768f29194a776df000b664
Cesar <cesarnsingi@hotmail.com>
2024-11-23 11:50:13 +0100

    add value

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -28,7 +28,7 @@ const AddProduct = () => {
     <div className='add-product'>
         <div className="addproduct-itemfield">
             <p>Product title</p>
-            <input type="text"  name='name' placeholder='Type here'/>
+            <input value={} type="text"  name='name' placeholder='Type here'/>
         </div>
         <div className="addproduct-price">
             <div className="addproduct-itemfield">
