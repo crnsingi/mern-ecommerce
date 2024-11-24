7692e708c59e8d99f4ee0d8da2f1734a64ad5dc8
Cesar <cesarnsingi@hotmail.com>
2024-11-24 01:44:41 +0100

    add 6 paragraph

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,12 @@ const ListProduct = () => {
     <div className='list-product'>
         <h1>All Produtcs List</h1>
         <div className="listproduct-format-main">
-            
+            <p>Products</p>
+            <p>Title</p>
+            <p>Old Price</p>
+            <p>New Price</p>
+            <p>Category</p>
+            <p>Remove</p>
         </div>
     </div>
   )
