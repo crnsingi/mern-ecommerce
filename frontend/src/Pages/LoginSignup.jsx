d6f1a62636d0ebeed85cff2b9a81e835f050f0f7
Cesar <cesarnsingi@hotmail.com>
2024-11-25 06:26:18 +0100

    import use sstate

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
@@ -6,11 +6,10 @@ const LoginSignup = () => {
   const [state,setState] = useState("Login");
 
 
-
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
           <input type="text" placeholder='Your Name' />
           <input type="email" placeholder='Email Address'/>
