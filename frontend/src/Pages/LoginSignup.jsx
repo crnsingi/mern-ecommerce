6cfc0f622624d4f31e2368db39342ce12ffe38ba
Cesar <cesarnsingi@hotmail.com>
2024-11-25 12:02:36 +0100

    value form data passoword

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -33,7 +33,7 @@ const LoginSignup = () => {
         <div className="loginsignup-fields">
           {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Your Name' />:<></>}
           <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Email Address'/>
-          <input name='password' type="password" placeholder='Password' />
+          <input name='password' value={formData.password} type="password" placeholder='Password' />
         </div>
         <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
         {state==="Sign Up"
