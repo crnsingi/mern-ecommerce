8ebfb10579b4856c68eee93122cd89ab1c13aea8
Cesar <cesarnsingi@hotmail.com>
2024-11-25 12:32:01 +0100

    responseData

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -29,7 +29,7 @@ const LoginSignup = () => {
         'Content-Type':'application/json',
         },
         body: JSON.stringify(formData),
-      }).then((response))=> response.json()).then((data)=>)
+      }).then((response)=> response.json()).then((data)=>responseData=data)
       })
     }
   }
